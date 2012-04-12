0c86d6b5fe337da530e891c92e35b7e4ff317d5a
Marc Abramowitz <marc@marc-abramowitz.com>
2012-04-12 16:15:30 -0700

    Remove copyright since I'm using a lot of other people's material

--- conf.py
+++ conf.py
@@ -49,7 +49,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Marc\'s PyCon 2012 Notes'
-copyright = u'2012, Marc Abramowitz'
+# copyright = u'2012, Marc Abramowitz'
+html_show_copyright = False
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
